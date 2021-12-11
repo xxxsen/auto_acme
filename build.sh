f927da57f459cdbbdc8d6c0233243a0acf1fe3dd
xxxtest <xxxtest@xx.co>
2021-12-11 21:33:45 +0800

    feat: arm support

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 VERSION=3.0.1
 cp Dockerfile Dockerfile.tmp
 sed -i "s/3.0.1/${VERSION}/g" ./Dockerfile.tmp
-docker build -f ./Dockerfile.tmp -t xxxsen/acme.sh:${VERSION} .
+docker buildx build -f ./Dockerfile.tmp -t xxxsen/acme.sh:${VERSION} . --platform=linux/amd64,linux/arm64 --push
 rm Dockerfile.tmp
